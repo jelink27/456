0e28dadb2fc9c78453afdf08f1ace0052e59929d
Jelink <jelink27@gmail.com>
2021-10-03 15:32:46 +0800

    add new

---
+++ 456.py
@@ -0,0 +1,15 @@
+x = 5
+print(x)
+'''整數 integer int'''
+'''浮點數(小數) float'''
+'''布林值 boolean bool (True,False )'''
+'''字串string str 'Jelink' '''
+price = 10
+pen = 0.38
+rain = True
+name = 'Jelink'
+
+print(price, pen, rain, name)
+
+name = input('請輸入名子: ')
+print('hi', name)
